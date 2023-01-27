2366e09dc3ca0b36ba2fed3b3af2c60b84d829dd
jherrera <jherrera@ingeneo.local>
2023-01-26 21:07:49 -0500

    main scadfoldding to handle MVVM with prism framework

---
+++ XamarinPrismTemplateForMac/XamarinPrismTemplateForMac/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Prism.Navigation;
+
+namespace XamarinPrismTemplateForMac.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase, INavigatedAware
+    {
+        private INavigationService _navigationService;
+
+        public MainPageViewModel(
+            INavigationService navigationService) : base(navigationService)
+        {
+            Title = "CNN en Español...";
+            this._navigationService = navigationService;
+        }
+    }
+}
